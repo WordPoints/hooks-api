fcfc0f34d8fb06dc209390ec327fd016b60c7546
JDGrimes <jdg@codesymphony.co>
2015-11-25 17:22:06 -0500

    Tests for the data type classes
    This actually gives us near 100% coverage (despite what code coverage
    tools might say).

---
+++ tests/phpunit/includes/classes/mock/data/type.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Mock data type class for the PHPUnit tests.
+ *
+ * @package wordpoints-hooks-api
+ * @since 1.0.0
+ */
+
+/**
+ * Mock data type class for the PHPUnit tests.
+ *
+ * @since 1.0.0
+ */
+class WordPoints_PHPUnit_Mock_Data_Type extends WordPoints_Data_Type {
+
+	/**
+	 * @since 1.0.0
+	 */
+	public function validate_value( $value ) {
+		return $value;
+	}
+}
+
+// EOF
