ed9fa2be05bc4ceebe66f366118ce5949ac8a3f2
JDGrimes <jdg@codesymphony.co>
2015-11-30 17:30:49 -0500

    Provide better support for accessing sub-apps during init
    When a sub-app is first accessed an action is fired to initialize it.
    However, it becomes impossible to access the sub app during the
    initialization process, because the `__get()` method cannot be called
    recursively. We work around this.

--- src/includes/classes/app.php
+++ src/includes/classes/app.php
@@ -86,6 +86,15 @@ class WordPoints_App {
 	protected $did_init = array();
 
 	/**
+	 * Whether properties are allowed to be set.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var bool
+	 */
+	protected $allow_set = false;
+
+	/**
 	 * @since 1.0.0
 	 */
 	public function __construct( $slug, $parent = null ) {
@@ -134,6 +143,21 @@ class WordPoints_App {
 			&& $this->should_do_registry_init( $sub )
 		) {
 
+			$is_protected_property = array_key_exists( $var, get_object_vars( $this ) );
+
+			// When the below action is called it is possible that some hooked code
+			// may attempt to access the property that is being initialized. However,
+			// the __get() function will not be called recursively, meaning that
+			// instead of the true value the code will get null. To solve this, we
+			// temporarily set this as a public property. However, we can't do this
+			// for properties that the class has declared, because otherwise it would
+			// be possible to modify and then unset protected class properties.
+			if ( ! $is_protected_property ) {
+				$this->allow_set = true;
+				$this->$var = $sub;
+				$this->allow_set = false;
+			}
+
 			/**
 			 * Initialization of an app registry.
 			 *
@@ -146,6 +170,22 @@ class WordPoints_App {
 			 */
 			do_action( "wordpoints_init_app_registry-{$this->full_slug}-{$var}", $sub );
 
+			// Because this property was public, it's possible that it might have
+			// been modified inadvertently. If this happens we give a warning.
+			if ( $sub !== $this->$var ) {
+				_doing_it_wrong(
+					__METHOD__
+					, esc_html( "Do not modify the {$var} property directly." )
+					, '1.0.0'
+				);
+			}
+
+			if ( ! $is_protected_property ) {
+				$this->allow_set = true;
+				unset( $this->$var );
+				$this->allow_set = false;
+			}
+
 			$this->did_init[ $var ] = true;
 		}
 
@@ -157,11 +197,15 @@ class WordPoints_App {
 	 */
 	public function __set( $var, $value ) {
 
-		_doing_it_wrong(
-			__METHOD__
-			, 'Sub apps must be registered using $app->sub_apps->register().'
-			, '1.0.0'
-		);
+		if ( $this->allow_set ) {
+			$this->$var = $value;
+		} else {
+			_doing_it_wrong(
+				__METHOD__
+				, 'Sub apps must be registered using $app->sub_apps->register().'
+				, '1.0.0'
+			);
+		}
 	}
 
 	/**
@@ -169,11 +213,15 @@ class WordPoints_App {
 	 */
 	public function __unset( $var ) {
 
-		_doing_it_wrong(
-			__METHOD__
-			, 'Sub apps must be deregistered using $app->sub_apps->deregister().'
-			, '1.0.0'
-		);
+		if ( $this->allow_set ) {
+			unset( $this->$var );
+		} else {
+			_doing_it_wrong(
+				__METHOD__
+				, 'Sub apps must be deregistered using $app->sub_apps->deregister().'
+				, '1.0.0'
+			);
+		}
 	}
 
 	/**
--- tests/phpunit/tests/classes/app.php
+++ tests/phpunit/tests/classes/app.php
@@ -14,7 +14,7 @@
  *
  * @covers WordPoints_App
  */
-class WordPoints_App_Test extends WP_UnitTestCase {
+class WordPoints_App_Test extends WordPoints_PHPUnit_TestCase {
 
 	/**
 	 * Test that it calls an action when it is constructed.
@@ -307,6 +307,182 @@ class WordPoints_App_Test extends WP_UnitTestCase {
 		unset( $app->sub );
 
 		$this->assertInstanceOf( 'WordPoints_PHPUnit_Mock_Object', $app->sub );
+	}
+
+	/**
+	 * Test when first getting a sub-app it is possible to access it in the action.
+	 *
+	 * @since 1.0.0
+	 */
+	public function test_get_registry_sub_app_access_sub_app() {
+
+		$this->mock_apps();
+
+		$apps = WordPoints_App::$main = new WordPoints_App( 'apps' );
+
+		$apps->sub_apps->register( 'test', 'WordPoints_App' );
+
+		$app = $apps->sub_apps->get( 'test' );
+
+		add_action(
+			'wordpoints_init_app_registry-test-registry'
+			, array( $this, 'action_get_sub_app' )
+		);
+
+		$mock = new WordPoints_Mock_Filter;
+
+		add_action(
+			'wordpoints_init_app_registry-test-registry'
+			, array( $mock, 'action' )
+		);
+
+		$this->assertTrue(
+			$app->sub_apps->register( 'registry', 'WordPoints_Class_Registry' )
+		);
+
+		$registry = $app->registry;
+
+		$this->assertInstanceOf( 'WordPoints_Class_Registry', $registry );
+
+		$this->assertEquals( 1, $mock->call_count );
+
+		$app->sub_apps->deregister( 'registry' );
+
+		$this->assertNull( $app->registry );
+	}
+
+	/**
+	 * Accesses a sub app.
+	 *
+	 * @since 1.0.0
+	 */
+	public function action_get_sub_app() {
+
+		$app = wordpoints_apps()->test;
+
+		$this->assertInstanceOf( 'WordPoints_Class_Registry', $app->registry );
+	}
+
+	/**
+	 * Test when first getting a sub-app a warning is issued if it is modified during
+	 * the action.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @expectedIncorrectUsage WordPoints_App::__get
+	 */
+	public function test_get_registry_sub_app_modify_sub_app() {
+
+		$this->mock_apps();
+
+		$apps = WordPoints_App::$main = new WordPoints_App( 'apps' );
+
+		$apps->sub_apps->register( 'test', 'WordPoints_App' );
+
+		$app = $apps->sub_apps->get( 'test' );
+
+		add_action(
+			'wordpoints_init_app_registry-test-registry'
+			, array( $this, 'action_modify_sub_app' )
+		);
+
+		$mock = new WordPoints_Mock_Filter;
+
+		add_action(
+			'wordpoints_init_app_registry-test-registry'
+			, array( $mock, 'action' )
+		);
+
+		$this->assertTrue(
+			$app->sub_apps->register( 'registry', 'WordPoints_Class_Registry' )
+		);
+
+		$registry = $app->registry;
+
+		$this->assertInstanceOf( 'WordPoints_Class_Registry', $registry );
+
+		$this->assertEquals( 1, $mock->call_count );
+
+		$app->sub_apps->deregister( 'registry' );
+
+		$this->assertNull( $app->registry );
+	}
+
+	/**
+	 * Modifies a sub app.
+	 *
+	 * @since 1.0.0
+	 */
+	public function action_modify_sub_app() {
+
+		$app = wordpoints_apps()->test;
+
+		$this->assertInstanceOf( 'WordPoints_Class_Registry', $app->registry );
+
+		$app->registry = 'a';
+
+		$this->assertEquals( 'a', $app->registry );
+	}
+
+	/**
+	 * Test when some sub-app shares the name of a protected property.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @expectedException PHPUnit_Framework_Error_Notice
+	 * @expectedExceptionMessage Undefined property: WordPoints_App::$parent
+	 *
+	 * @expectedIncorrectUsage WordPoints_App::__set
+	 */
+	public function test_get_registry_sub_app_access_restricted_property() {
+
+		$this->mock_apps();
+
+		$apps = WordPoints_App::$main = new WordPoints_App( 'apps' );
+
+		$apps->sub_apps->register( 'test', 'WordPoints_App' );
+
+		$app = $apps->sub_apps->get( 'test' );
+
+		add_action(
+			'wordpoints_init_app_registry-test-parent'
+			, array( $this, 'action_access_protected_sub_app' )
+		);
+
+		$mock = new WordPoints_Mock_Filter;
+
+		add_action(
+			'wordpoints_init_app_registry-test-parent'
+			, array( $mock, 'action' )
+		);
+
+		$this->assertTrue(
+			$app->sub_apps->register( 'parent', 'WordPoints_Class_Registry' )
+		);
+
+		$registry = $app->parent;
+
+		$this->assertInstanceOf( 'WordPoints_Class_Registry', $registry );
+
+		$this->assertEquals( 1, $mock->call_count );
+
+		$app->sub_apps->deregister( 'parent' );
+
+		$this->assertNull( $app->parent );
+	}
+
+	/**
+	 * Accesses a sub app that is also a declared protected class property.
+	 *
+	 * @since 1.0.0
+	 */
+	public function action_access_protected_sub_app() {
+
+		$app = wordpoints_apps()->test;
+		// Attempt to modify it.
+		$app->parent = 'a';
+
+		$this->assertNull( $app->parent );
 	}
 }
 
