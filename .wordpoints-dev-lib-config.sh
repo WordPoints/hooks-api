3199d1869d08bfdf4a03d31706cb4d857426b423
JDGrimes <jdg@codesymphony.co>
2016-02-08 15:26:33 -0500

    Test running Codeception tests with Travis CI
    See #92

---
+++ .wordpoints-dev-lib-config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+export DO_WP_CEPT=$(if [[ $TRAVIS_PHP_VERSION != '5.4' ]]; then echo 1; else echo 0; fi)
+
+wpcept-setup() {
+
+	if [[ $DO_WP_CEPT == 0 ]]; then
+		return;
+	fi
+
+	if [[ $WP_VERSION != '4.4' ]]; then
+		export DISPLAY=:99.0
+ 		sh -e /etc/init.d/xvfb start
+ 	fi
+
+	composer require --prefer-source codeception/codeception:2.1.4
+	composer require --prefer-source lucatume/wp-browser:master@dev
+
+	# We start the server up early so that it has time to prepare.
+	php -S 127.0.0.1:8000 -t "$WP_CORE_DIR" >/dev/null 2>&1 &
+}
+
+wpcept-run() {
+
+	if [[ $DO_WP_CEPT == 0 ]]; then
+		echo Not running codecept tests.
+	fi
+
+	sed -i "s/http:\/\/wptests.local/127.0.0.1:8000/" codeception.yml
+
+	phantomjs --webdriver=4444
+
+	vendor/bin/wpcept run
+}
