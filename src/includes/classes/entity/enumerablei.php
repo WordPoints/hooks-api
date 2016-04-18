5a24d8ba458f5c3aa6f9f2ef29006b0be3db8725
JDGrimes <jdg@codesymphony.co>
2016-04-18 17:59:22 -0400

    Fix enumerable entity interface docs
    Discovered that these weren’t right while working on #124.

--- src/includes/classes/entity/enumerablei.php
+++ src/includes/classes/entity/enumerablei.php
@@ -9,22 +9,21 @@
 
 /**
  * Implemented by entities that are enumerable.
+ * 
+ * Indicates that only a finite number of entities of this type will exist, and
+ * that it is therefore appropriate to display a list of the entities in a select
+ * box in the UI.
  *
  * @since 1.0.0
  */
 interface WordPoints_Entity_EnumerableI {
 
 	/**
-	 * Get a list of the predefined values this attribute can have.
-	 *
-	 * The list should contain an array of data for each value, with the value itself
-	 * accessible via the 'value' key. The slug for the value, if different from the
-	 * value itself, should be supplied as the 'slug'. A human-readable 'label' may
-	 * optionally be supplied.
+	 * Get a list of the entities of this type.
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return array[] An array of data for each value, indexed by value slug.
+	 * @return array The entity objects/IDs.
 	 */
 	public function get_enumerated_values();
 }
