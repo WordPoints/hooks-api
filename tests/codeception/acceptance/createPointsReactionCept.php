9a55986bd70affaae063c6f6973fb310f2264f5d
JDGrimes <jdg@codesymphony.co>
2016-05-28 08:28:37 -0400

    Test that the "Add New" button is disabled
    The button should be disabled when we add a new reaction, and then
    re-enabled after that reaction is saved.
    
    See #92

--- tests/codeception/acceptance/createPointsReactionCept.php
+++ tests/codeception/acceptance/createPointsReactionCept.php
@@ -18,11 +18,13 @@ $I->see( 'Events' );
 $I->see( 'Register', '#points-user_register' );
 $I->click( 'Add New', '#points-user_register' );
 $I->waitForNewReaction();
+$I->seeElement( '#points-user_register .add-reaction[disabled]' );
 $I->fillField( 'description', 'Registering.' );
 $I->fillField( 'log_text', 'Registration.' );
 $I->fillField( 'points', '10' );
 $I->click( 'Save', '#points-user_register' );
 $I->waitForJqueryAjax();
 $I->see( 'Your changes have been saved.', '#points-user_register .messages' );
+$I->cantSeeElement( '#points-user_register .add-reaction[disabled]' );
 
 // EOF
