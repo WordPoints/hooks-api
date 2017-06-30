5e64274708b49dc9367a9c79bd569ee664e8cae0
JDGrimes <jdg@codesymphony.co>
2017-06-30 12:09:01 -0400

    Remove post term entity code moved to core
    See WordPoints/wordpoints#461

--- src/includes/actions.php
+++ src/includes/actions.php
@@ -7,8 +7,4 @@
  * @since 1.0.0
  */
 
-add_action( 'wordpoints_init_app_registry-apps-entities', 'wordpoints_taxonomy_entities_init' );
-
-add_action( 'wordpoints_register_post_type_entities', 'wordpoints_register_post_type_taxonomy_entities' );
-
 // EOF
